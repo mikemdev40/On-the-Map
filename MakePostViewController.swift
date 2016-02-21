1a365b20c206894274949edc6a1f430b420d84c2
mike miller <mikemdev40@gmail.com>
2016-02-21 14:07:03 -0500

    feat: add view controller for posting

---
+++ MakePostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MakePostViewController.swift
+//  On the Map
+//
+//  Created by Michael Miller on 2/21/16.
+//  Copyright © 2016 MikeMiller. All rights reserved.
+//
+
+import UIKit
+
+class MakePostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
